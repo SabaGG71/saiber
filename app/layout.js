5a5b73951d3f40b65bd0ccfe796e050aabf07a90
Saba <sgachechiladze881@gmail.com>
2025-01-03 17:51:20 +0400

    Add anaylitcs

--- app/layout.js
+++ app/layout.js
@@ -4,7 +4,6 @@ import Footer from "./_components/Footer";
 import { MovingBorderDemo } from "./_components/MovingBorderDemo";
 import { Rethink_Sans } from "next/font/google";
 import cursorYou from "../public/pointer.svg";
-import Head from "next/head";
 export const metadata = {
   title: "Saiber | Official",
 };
@@ -17,10 +16,10 @@ const rethink_Sans = Rethink_Sans({
 export default function RootLayout({ children }) {
   return (
     <html lang="en" className="!scroll-smooth">
-      <Head>
+      <head>
         <script
           async
-          src="https://www.googletagmanager.com/gtag/js?id=G-2PHPSGK1EQ"
+          src="https://www.googletagmanager.com/gtag/js?id=G-X553ZZF6YN"
         ></script>
         <script
           dangerouslySetInnerHTML={{
@@ -28,22 +27,11 @@ export default function RootLayout({ children }) {
               window.dataLayer = window.dataLayer || [];
               function gtag(){dataLayer.push(arguments);}
               gtag('js', new Date());
-              gtag('config', 'G-2PHPSGK1EQ');
+              gtag('config', 'G-X553ZZF6YN');
             `,
           }}
         ></script>
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-              (function(c,l,a,r,i,t,y){
-                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-              })(window, document, "clarity", "script", "po549l0gad");
-            `,
-          }}
-        ></script>
-      </Head>
+      </head>
       <body
         className={`bg-primary-900 max-w-full ${rethink_Sans.className} text-white antialiased`}
       >
